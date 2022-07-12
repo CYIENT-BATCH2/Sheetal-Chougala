a3eabb0651bcba8771533dc7fc377c73c892c363
SheetalChougala <108974064+SheetalChougala@users.noreply.github.com>
2022-07-12 14:04:22 +0530

    Create DAY_1_PROG_3.c

---
+++ DAY1/DAY_1_PROG_3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float r,a;
+	printf("enter radius of circle\n");
+	fflush(stdout);
+	scanf("%f",&r);
+	a=3.14*r*r;
+	printf("a=%f area of total area of circle\n",a);
+}
