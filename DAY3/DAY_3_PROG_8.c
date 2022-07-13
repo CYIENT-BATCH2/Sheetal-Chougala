643284b9f856882002934c3d282243a8d1bcd4a6
SheetalChougala <108974064+SheetalChougala@users.noreply.github.com>
2022-07-13 12:27:31 +0530

    Create DAY_3_PROG_8.c

---
+++ DAY3/DAY_3_PROG_8.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter the charcter\n");
+	fflush(stdout);
+	scanf("%c",&ch);
+((ch>='a'||ch>='A')&&(ch<='z'||ch<='Z'))?printf("alphabet\n"):printf("not alphabet\n");
+
+}
