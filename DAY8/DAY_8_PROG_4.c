a1dbda8e2be61e618346ca3eadf22586498830fb
SheetalChougala <108974064+SheetalChougala@users.noreply.github.com>
2022-07-24 13:34:51 +0530

    Create DAY_8_PROG_4.c

---
+++ DAY8/DAY_8_PROG_4.c
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+#include<stdio.h>
+int toggle(int data,int pos)
+{
+	data=data^(1<<pos);
+	return data;
+
+}
+int main()
+{
+	int pos,data,result;
+	printf("enter the data\n");
+	fflush(stdout);
+	scanf("%d",&data);
+	printf("enter the pos\n");
+	scanf("%d",&pos);
+
+
+}
+
